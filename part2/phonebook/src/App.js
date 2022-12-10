cbfafc56415ef906868797fcbc10d3ac2211f1d9
Evelyn Birnzain <e11704999@student.tuwien.ac.at>
2022-12-10 17:45:13 +0000

    Part 2: ex 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import axios from "axios";
 
 const Filter = (props) => {
     return (
@@ -39,12 +40,16 @@ const PersonForm = (props) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-1234567'}
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [searchString, setSearchString] = useState('')
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/persons').then(response => {
+            setPersons(response.data)
+        })
+    }, [])
 
     const handleAddName = (event) => {
         event.preventDefault()
