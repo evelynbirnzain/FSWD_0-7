126ca8c0ed0fd9741fcc888f572ec1300b3cdeb2
Evelyn Birnzain <e11704999@student.tuwien.ac.at>
2022-12-11 17:49:42 +0000

    Part 3: 3.9 backend changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(cors())
 
 let entries = [{"id": 1, "name": "Arto Hellas", "number": "040-123456"},
     {"id": 2, "name": "Ada Lovelace", "number": "39-44-5323523"},
@@ -74,7 +76,7 @@ app.post('/api/persons', (request, response) => {
     response.json(entry)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
