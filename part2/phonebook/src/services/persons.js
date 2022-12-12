cfad9e3bea36707a0ade5d1f48101f5054f05830
Evelyn Birnzain <e11704999@student.tuwien.ac.at>
2022-12-12 16:50:23 +0000

    Part 2: set up proxy

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
@@ -20,8 +20,6 @@ const update = (id, newObject) => {
     const request = axios.put(`${baseUrl}/${id}`, newObject)
     return request.then(response => response.data)
 }
-
-
 
 export default {
     getAll, create, remove, update
