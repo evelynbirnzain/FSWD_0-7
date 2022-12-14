9254f643bfcbd9452be69474efe14d473c3e601a
Evelyn Birnzain <e11704999@student.tuwien.ac.at>
2022-12-14 20:13:33 +0000

    Part 4: 4.3-4.5

---
+++ utils/list_helper.js
@@ -0,0 +1,17 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0
+  return blogs.map(blog => blog.likes).reduce((sum, likes) => sum + likes)
+}
+
+const favoriteBlog = (blogs) => {
+  const maxLikes = blogs.map(blog => blog.likes).max()
+  return blogs.find(blog => blog.likes === maxLikes)
+}
+
+module.exports = {
+  dummy, totalLikes, favoriteBlog
+}
